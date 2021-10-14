937ceff045ee71676dd4a0964a1dec8127b625db
musayuksel <musayuxel@gmail.com>
2021-10-14 01:20:37 +0100

    feat:Add level-2 Search bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 import babyNames from "./babyNamesData.json";
 import BabyCard from "./components/BabyCard";
 
 function App() {
-  const babies = babyNames.map((baby) => (
+  const [searchedBaby, setSearcBaby] = useState(babyNames);
+  function searchFunc(e) {
+    setSearcBaby(() => {
+      return babyNames.filter((baby) =>
+        baby["name"].toLowerCase().includes(e.target.value.toLowerCase())
+      );
+    });
+  }
+
+  const babies = searchedBaby.map((baby) => (
     <BabyCard key={baby.id} name={baby.name} sex={baby.sex} />
   ));
 
   return (
     <div className="App">
+      <input
+        type="text"
+        name="nameSearch"
+        id="nameSearch"
+        placeholder="Search baby name"
+        onChange={searchFunc}
+      />
       <h1>Choose a name for you future...</h1>
       <section className="babyCardContainer">{babies}</section>
     </div>
